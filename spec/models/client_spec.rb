91e1716063e2dc48510914997991e69f9a34fd8e
Philipe Leandro <philipe-leandro@hotmail.com>
2024-09-19 22:50:47 +0000

    Refactor tests

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Client, type: :model do
-  describe "Validations" do
+RSpec.describe Client do
+  describe "validations" do
+    subject { build(:client) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:company_name) }
+    it { is_expected.to validate_presence_of(:phone) }
+    it { is_expected.to validate_presence_of(:cpf) }
+    it { is_expected.to validate_presence_of(:email) }
+
+    it { is_expected.to validate_uniqueness_of(:cpf).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:cnpj).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:email) }
+
+    it { is_expected.to allow_value('user@example.com').for(:email) }
+    it { is_expected.not_to allow_value('@com.br').for(:email) }
+    it { is_expected.not_to allow_value('user.com.br').for(:email) }
   end
 end
