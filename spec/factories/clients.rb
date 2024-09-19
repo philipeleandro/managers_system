520879c8f17a68573fb527e306f1f360b24340d2
Izabellyrb <izabelly.rb@hotmail.com>
2024-09-19 01:35:24 +0000

    add client factory + factory config

---
+++ spec/factories/clients.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    name { 'João Doe' }
+    company_name { 'Doe Ltda' }
+    phone { '65987064859' }
+    email { 'joao@doe.com' }
+    cpf { '62398144151' }
+    cnpj { '10896777000130' }
+    status_id { 0 }
+  end
+end
