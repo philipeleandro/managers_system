dcfd44d28edbb6d55c903901eeffa62c63fc6b14
Izabellyrb <izabelly.rb@hotmail.com>
2024-09-21 18:23:55 +0000

    rubocop fix

--- db/migrate/20240921134546_change_status_id_to_status.rb
+++ db/migrate/20240921134546_change_status_id_to_status.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class ChangeStatusIdToStatus < ActiveRecord::Migration[7.2]
   def change
-
-    change_column(:hirers, :status_id, :string, :default => :active, null: false)
+    change_column(:hirers, :status_id, :string, default: :active, null: false)
     rename_column(:hirers, :status_id, :status)
   end
 end
