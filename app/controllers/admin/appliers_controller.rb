609db4f4b2892652f1f58c5be615b19d7b7861b5
Izabellyrb <izabelly.rb@hotmail.com>
2024-09-27 02:32:27 +0000

    fix heritage

--- app/controllers/admin/appliers_controller.rb
+++ app/controllers/admin/appliers_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Admin
-  class AppliersController < ApplicationController
+  class AppliersController < Admin::ApplicationController
     before_action :authorize_by_action, only: %i[index create]
 
     def index
