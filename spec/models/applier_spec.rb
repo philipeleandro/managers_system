842bbf95383a711f2398a8100707286d58d2cd3e
Philipe Leandro <philipe-leandro@hotmail.com>
2024-10-01 23:11:32 +0000

    Fix broken tests

--- spec/models/applier_spec.rb
+++ spec/models/applier_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Applier do
     subject { build(:applier) }
 
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:cv_link) }
+    it { is_expected.to validate_presence_of(:attachment) }
     it { is_expected.to validate_presence_of(:phone) }
     it { is_expected.to validate_presence_of(:city) }
     it { is_expected.to validate_presence_of(:state) }
@@ -18,10 +18,6 @@ RSpec.describe Applier do
     it { is_expected.not_to allow_value('@com.br').for(:email) }
     it { is_expected.not_to allow_value('user.com.br').for(:email) }
 
-    it { is_expected.to allow_value('https://www.example.com.br').for(:cv_link) }
-    it { is_expected.to allow_value('http://www.example.com.br').for(:cv_link) }
-    it { is_expected.not_to allow_value('www.example.com').for(:cv_link) }
-    it { is_expected.not_to allow_value('httpsss://www.example.com.br').for(:cv_link) }
-    it { is_expected.not_to allow_value('teste.com').for(:cv_link) }
+    it { is_expected.to have_one_attached(:attachment) }
   end
 end
