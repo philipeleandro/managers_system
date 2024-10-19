dd3bc4712801873d410ad15084a87c993c1715e0
Izabellyrb <izabelly.rb@hotmail.com>
2024-10-19 20:14:59 -0300

    Revert "Add services to controllers"
    This reverts commit 0e3225a73c57e8d252c35363310d77a864f475f0.

--- app/controllers/admin/welcomes_controller.rb
+++ app/controllers/admin/welcomes_controller.rb
@@ -5,17 +5,17 @@ module Admin
     before_action :authorize_by_action, only: %i[index]
 
     def index
-      instance = Admin::Welcome::List.new(
-        dashboard: dashboard,
-        order: order,
-        params: params,
-        scoped_resource: User.all
-      ).call
+      search_term = params[:search].to_s.strip
+      resources = filter_resources(User.all, search_term: search_term)
+      resources = apply_collection_includes(resources)
+      resources = order.apply(resources)
+      resources = paginate_resources(resources)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
 
       render locals: {
-        resources: instance[:resources],
-        search_term: instance[:search_term],
-        page: instance[:page],
+        resources: resources,
+        search_term: search_term,
+        page: page,
         show_search_bar: true
       }
     end
